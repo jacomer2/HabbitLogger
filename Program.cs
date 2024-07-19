d363f2924af53ff59fdaa6f97dcbc3db95fb749e
jacomer2 <69131100+jacomer2@users.noreply.github.com>
2024-07-19 15:25:33 -0400

    added hobby class, etc

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Data.SqlClient;
 using Microsoft.Data.Sqlite;
 using static System.Formats.Asn1.AsnWriter;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 class Database
 {
@@ -26,7 +27,7 @@ class Database
         }
         catch (SqliteException ex)
         {
-            Console.WriteLine(ex.Message);
+            //Console.WriteLine(ex.Message);
         }
 
     }
@@ -59,6 +60,78 @@ class Database
     public static void deleteDB(int id)
     {
 
+        try
+        {
+            using var connection = new SqliteConnection(@"Data Source=C:\Users\jacom\Documents\LearnC#\HabbitLogger\db\pub.db");
+            connection.Open();
+
+            string sql = "DELETE FROM habit WHERE id = @id";
+            SqliteCommand command2 = new SqliteCommand(sql, connection);
+            command2.Parameters.AddWithValue("@id", id);
+
+            command2.ExecuteNonQuery();
+
+            Console.WriteLine("Deleted habit entry from table");
+
+        }
+        catch (SqliteException ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+
+        return;
+    }
+
+    public static void updateDB(int id, int quantity, DateOnly date)
+    {
+        try
+        {
+            using var connection = new SqliteConnection(@"Data Source=C:\Users\jacom\Documents\LearnC#\HabbitLogger\db\pub.db");
+            connection.Open();
+
+            string sql = "UPDATE habit SET date = @date, quantity = @quantity WHERE id = @id";
+            SqliteCommand command2 = new SqliteCommand(sql, connection);
+            command2.Parameters.AddWithValue("@date", date);
+            command2.Parameters.AddWithValue("@quantity", quantity);
+            command2.Parameters.AddWithValue("@id", id);
+
+
+            command2.ExecuteNonQuery();
+
+            Console.WriteLine("Updated habit entry to table");
+
+        }
+        catch (SqliteException ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+
+
+
+        return;
+    }
+
+    public static void printRecords()
+    {
+        try
+        {
+            using var connection = new SqliteConnection(@"Data Source=C:\Users\jacom\Documents\LearnC#\HabbitLogger\db\pub.db");
+            connection.Open();
+
+            string sql2 = "INSERT INTO habit (date, quantity) VALUES (@date, @quantity)";
+            SqliteCommand command2 = new SqliteCommand(sql2, connection);
+
+
+            command2.ExecuteNonQuery();
+
+            Console.WriteLine("Added habit entry to table");
+
+        }
+        catch (SqliteException ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+
         return;
     }
 
@@ -72,44 +145,53 @@ class Program
     {
         Database.createDB();
 
-        Console.WriteLine("MAIN MENU");
-        Console.WriteLine();
-        Console.WriteLine("What would you like to do?");
-        Console.WriteLine();
-        Console.WriteLine("Type 0 to Close Application");
-        Console.WriteLine("Type 1 to View All Records");
-        Console.WriteLine("Type 2 to Insert Record");
-        Console.WriteLine("Type 3 to Delete Record");
-        Console.WriteLine("Type 4 to Update Record");
-        Console.WriteLine("-----------------------------------------");
-
-        String res = Console.ReadLine();
-
-        res ??= "";
-
-        switch (res)
+        bool quit = false;
+        
+        while(!quit)
         {
-            case "0":
-                return;
-            case "1":
-                Records();
-                break;
-            case "2":
-                Insert();
-                break;
-            case "3":
-                Delete();
-                break;
-            case "4":
-                Update();
-                break;
-            default:
-                Console.WriteLine("Invalid Input");
-                Console.WriteLine("");
-                Console.WriteLine("");
-                break;
+            Console.WriteLine("MAIN MENU");
+            Console.WriteLine();
+            Console.WriteLine("What would you like to do?");
+            Console.WriteLine();
+            Console.WriteLine("Type 0 to Close Application");
+            Console.WriteLine("Type 1 to View All Records");
+            Console.WriteLine("Type 2 to Insert Record");
+            Console.WriteLine("Type 3 to Delete Record");
+            Console.WriteLine("Type 4 to Update Record");
+            Console.WriteLine("-----------------------------------------");
 
+            string res = Console.ReadLine();
+
+            res ??= "";
+
+            switch (res)
+            {
+                case "0":
+                    quit = true;
+                    return;
+                case "1":
+                    Records();
+                    break;
+                case "2":
+                    Insert();
+                    break;
+                case "3":
+                    Delete();
+                    break;
+                case "4":
+                    Update();
+                    break;
+                default:
+                    Console.WriteLine("Invalid Input");
+                    Console.WriteLine("");
+                    Console.Write("Press any key to continue");
+                    Console.ReadKey();
+                    Console.Clear();
+                    break;
+
+            }
         }
+        
 
     }
 
@@ -125,6 +207,12 @@ class Program
         {
             string dateStr = Console.ReadLine();
             dateStr ??= "";
+
+            if (dateStr.Equals("0"))
+            {
+                Console.Clear();
+                return;
+            }
 
             try
             {
@@ -150,9 +238,16 @@ class Program
 
             quantStr ??= "";
 
-            if (int.TryParse(quantStr, out quantity) && quantity > 0)
+            if (quantStr.Equals("0"))
+            {
+                Console.Clear();
+                return;
+            }
+
+            if (int.TryParse(quantStr, out quantity) && quantity >= 0)
             {
                 passed = true;
+
             }
             else
             {
@@ -163,17 +258,154 @@ class Program
 
         Database.insertDB(quantity, date);
 
+        Console.WriteLine();
+        Console.Write("Press any key to continue");
+
+        Console.ReadKey();
+
+        Console.Clear();
+
         return;
     }
 
     static void Delete()
     {
 
+        Console.WriteLine("Please enter the id of the entry you'd like to delete. Type 0 to return to the Main Menu.");
+
+        bool passed = false;
+
+        while (!passed)
+        {
+            string idStr = Console.ReadLine();
+
+            idStr ??= "";
+
+            if (idStr.Equals("0"))
+            {
+                Console.Clear();
+                return;
+            }
+
+            if (int.TryParse(idStr, out int id))
+            {
+                Database.deleteDB(id);
+                passed = true;
+            }
+            else
+            {
+                // Conversion failed, handle the error.
+                Console.WriteLine("Invalid id. Enter a valid id. Type 0 to return to the Main Menu.");
+            }
+        }
+
+        Console.WriteLine();
+        Console.Write("Press any key to continue");
+
+        Console.ReadKey();
+
+        Console.Clear();
+
         return;
     }
 
     static void Update()
     {
+        bool passed = false;
+        int id = 0;
+        int quantity = 0;
+        DateOnly date = new DateOnly();
+
+        Console.WriteLine("Enter the id of the entry to update");
+
+        while (!passed)
+        {
+            string idStr = Console.ReadLine();
+
+            idStr ??= "";
+
+            if (idStr.Equals("0"))
+            {
+                Console.Clear();
+                return;
+            }
+
+            if (int.TryParse(idStr, out id))
+            {
+                passed = true;
+            }
+            else
+            {
+                // Conversion failed, handle the error.
+                Console.WriteLine("Invalid id. Enter a valid id. Type 0 to return to the Main Menu.");
+            }
+        }
+
+        passed = false;
+
+        Console.WriteLine("Please enter the date: (Format: dd-mm-yyyy). Type 0 to return to the Main Menu.");
+
+        while (!passed)
+        {
+            string dateStr = Console.ReadLine();
+            dateStr ??= "";
+
+            if (dateStr.Equals("0"))
+            {
+                Console.Clear();
+                return;
+            }
+
+            try
+            {
+                string dateFormat = "dd-MM-yyyy";
+                date = DateOnly.ParseExact(dateStr, dateFormat);
+
+                passed = true;
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Invalid input. Please enter a valid date. (Format: dd-mm-yyyy)");
+            }
+        }
+
+        passed = false;
+
+        Console.WriteLine("Please enter the cups of water drank. Type 0 to return to the Main Menu.");
+
+
+        while (!passed)
+        {
+            string quantStr = Console.ReadLine();
+
+            quantStr ??= "";
+
+            if (quantStr.Equals("0"))
+            {
+                Console.Clear();
+                return;
+            }
+
+            if (int.TryParse(quantStr, out quantity) && quantity >= 0)
+            {
+                passed = true;
+
+            }
+            else
+            {
+                // Conversion failed, handle the error.
+                Console.WriteLine("Invalid quantity");
+            }
+        }
+
+        Database.updateDB(id, quantity, date);
+
+        Console.WriteLine();
+        Console.Write("Press any key to continue");
+
+        Console.ReadKey();
+
+        Console.Clear();
 
         return;
     }
@@ -185,12 +417,13 @@ class Program
     }
 }
 
-class Converesions
+class Habit 
 {
-    static DateOnly? StringToDate(string dateStr)
-    {
+    private int id; 
+    private int quantity; 
+    private DateOnly date;
 
-
-        return null;
-    }
+    public int Id { get => id; set => id = value; }
+    public int Quantity { get => quantity; set => quantity = value; }
+    public DateOnly Date { get => date; set => date = value; }
 }
